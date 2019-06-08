c7bd474a66ae2b1a4b10d5d1f1afc0f9d1e62360
IanTorrente <ianpaolotorrente@gmail.com>
2019-06-08 11:18:52 -0700

    get ready for inital push to heroku

--- src/server.js
+++ src/server.js
@@ -1,6 +1,23 @@
+const express = require('express');
 const app = require('./app');
 const { PORT } = require('./config');
+
+app.get('/api/*', (req, res) => {
+  res.json({ ok: true });
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
 });
+
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+
+module.exports = { app };
