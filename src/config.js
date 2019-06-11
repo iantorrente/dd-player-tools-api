f6531b1d997522b9f41091dacfae135465959e5f
IanTorrente <ianpaolotorrente@gmail.com>
2019-06-11 02:01:51 -0700

    change client_origin to production origin

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 3000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  CLIENT_ORIGIN: 'http://localhost:3000',
+  CLIENT_ORIGIN: 'https://dnd-character-creator.now.sh',
   DB_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/dd-player-tools',
 }
